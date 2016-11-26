0e36eb85371f081789c3c84ecc36228fc13a3a29
mizworski <mi360968@students.mimuw.edu.pl>
2016-11-26 14:09:14 +0100

    Removed unnecessary files.

--- smalltown.cc
+++
@@ -1,1 +0,0 @@
-#include "smalltown.h"
--- smalltown.h
+++ smalltown.h
@@ -1,4 +1,8 @@
 #ifndef HORROR_SMALLTOWN_H
 #define HORROR_SMALLTOWN_H
 
+class SmallTown<typename M, typename U, U t0, U t1, typename... C> {
+
+};
+
 #endif //HORROR_SMALLTOWN_H
