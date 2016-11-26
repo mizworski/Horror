6ba06fa2c6a011d8ff66b5484e0247eacfb8a60d
Michał Kuźba <kmichael00968@gmail.com>
2016-11-26 16:41:49 +0100

    Implement citizen class

--- citizen.h
+++ citizen.h
@@ -1,4 +1,41 @@
+#include <iostream>
+#include <cassert>
+
 #ifndef HORROR_CITIZEN_H
 #define HORROR_CITIZEN_H
+
+template <typename T, int lower, int upper>
+class Citizen {
+private:
+    T health;
+    T age;
+public:
+    Citizen(T health, T age) : health(health), age(age) { assert(age >= lower && age <= upper); }
+    T getHealth() { return health; }
+    T getAge() { return age; }
+    void getDamage(T damage) { age = std::max(0, age - damage); }
+};
+
+template <typename T>
+class Adult : public Citizen<T, 18, 100> {
+    using Citizen<T, 18, 100>::Citizen;
+
+};
+
+template <typename T>
+class Teenager : public Citizen<T, 11, 17> {
+public:
+    using Citizen<T, 11, 17>::Citizen;
+};
+
+template <typename T>
+class Sheriff : public Citizen<T, 18, 100> {
+private:
+    T attackPower;
+public:
+    T getAttackPower() { return attackPower; }
+    Sheriff(T health, T age, T attackPower) : Citizen<T, 18, 100>::Citizen(health, age), attackPower(attackPower) {};
+};
+
 
 #endif //HORROR_CITIZEN_H
